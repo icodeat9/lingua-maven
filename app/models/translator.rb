c0927c22e98432092f190dc4a75e8aac6d64724c
Enrique <icodeat9>
2022-03-23 23:11:45 -0400

    add translator model, was missing from previus commit

---
+++ app/models/translator.rb
@@ -0,0 +1,16 @@
+class Translator < ApplicationRecord
+  validates :profile_enabled, acceptance: true, if: -> { slug.present? }
+  validates :slug, presence: true, if: -> { profile_enabled }
+
+  validate :bio_should_be_a_valid_json
+
+  private
+  def bio_should_be_a_valid_json
+    return unless bio.present?
+    begin
+      ActiveSupport::JSON.decode(bio)
+    rescue JSON::ParserError, TypeError => exception
+      errors.add(:bio, "should be a valid JSON")
+    end
+  end
+end
