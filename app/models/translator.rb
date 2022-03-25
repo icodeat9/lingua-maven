d15b413d44b108c1065f7a1558ff90f61ceba2de
Enrique <icodeat9>
2022-03-25 12:34:18 -0400

    remiving optional parameter from has-to-one assosiation and validate slug ascii

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -1,17 +1,27 @@
 class Translator < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   validates :profile_enabled, acceptance: true, if: -> { slug.present? }
   validates :slug, presence: true, if: -> { profile_enabled }
 
   validate :bio_should_be_a_valid_json
+  validate :slug_should_be_ascii
 
   private
   def bio_should_be_a_valid_json
     return unless bio.present?
+
     begin
       ActiveSupport::JSON.decode(bio)
     rescue JSON::ParserError, TypeError => exception
       errors.add(:bio, "should be a valid JSON")
+    end
+  end
+
+  def slug_should_be_ascii
+    return unless slug.present?
+
+    unless slug.ascii_only?
+      errors.add(:slug, "can't include non-ASCII characters.")
     end
   end
 end
--- test/models/translator_test.rb
+++ test/models/translator_test.rb
@@ -15,4 +15,20 @@ class TranslatorTest < ActiveSupport::TestCase
 
     assert_not translator.save, "Saved the translator with profile enabled true when slug is missing"
   end
+
+  test "should not save with slug containing non-ASCII characters" do
+    translator = Translator.new
+    translator.profile_enabled = true
+    translator.slug = 'piña@test.com'
+
+    assert_not translator.save, "Saved the translator containing non-ASCII characters"
+  end
+
+  test "should not save with without being assigned to an user" do
+    translator = Translator.new
+    translator.profile_enabled = true
+    translator.slug = 'pina@test.com'
+
+    assert_not translator.save, "Saved the translator without bbeing assigned to an user"
+  end
 end
